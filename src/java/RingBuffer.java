3382070ea3c2247d6a4ca6b960539842f513c2d3
Naman Sharma <namansharma18899@gmail.com>
2023-03-30 22:23:39 +0530

    adding java/ringbuffer inprg

--- src/java/RingBuffer.java
+++ src/java/RingBuffer.java
@@ -1,10 +1,47 @@
 import java.util.*;
 import java.io.*;
 
+class CircularBuffer {
+    private int DEFAULT_CAPACITY=10;
+    private int capacity;
+    private int readSequence;
+    private int writeSequence;
+    private ArrayList<Integer> data;
 
 
-class RingBuffer(){
-public static void main(String args[])throws IOException{
-System.out.println("SkeletonCode");
+
+    public CircularBuffer(int capacity) {
+        this.capacity = (capacity < 1) ? DEFAULT_CAPACITY : capacity;
+        this.data = new ArrayList<Integer>(this.capacity);
+        this.readSequence = 0;
+        this.writeSequence = -1;
+    }
+    
+    public Boolean Offer(int element){
+        boolean isFilled = (this.writeSequence - this.readSequence) + 1 == this.capacity;
+        if(!isFilled){
+            this.data.add((++this.writeSequence)%this.capacity, element);
+            return true;
+        }
+        return false;
+    }
+
+    public Integer Poll() {
+        boolean isEmpty = this.writeSequence < this.readSequence;
+        if(!isEmpty){
+            int obj = this.data.get(this.readSequence++);
+            return obj;
+        }
+        return null;
+    }
+
+
 }
+
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        System.out.println("SkeletonCode");
+    }
+
 }
