2ecd39a41ac8e792e5dadbe4d03fa5dc324fcfd4
Naman Sharma <namansharma18899@gmail.com>
2023-04-14 10:56:41 +0530

    added missing folders prjt structure

---
+++ algosc/src/main/java/io/algosc/AlgoscApplication.java
@@ -0,0 +1,12 @@
+package io.algosc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgoscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgoscApplication.class, args);
+	}
+}
---
+++ algosc/src/test/java/io/algosc/AlgoscApplicationTests.java
@@ -0,0 +1,13 @@
+package io.algosc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgoscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
