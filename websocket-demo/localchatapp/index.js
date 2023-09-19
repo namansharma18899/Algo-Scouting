ed6d3c47d7d79fdb508b285a3bec2bdc1631c33a
Naman Sharma <namansharma18899@gmail.com>
2023-09-19 12:23:23 +0530

    Dynamic change in message tiles

--- websocket-demo/localchatapp/index.js
+++ websocket-demo/localchatapp/index.js
@@ -5,7 +5,22 @@ import ws, { WebSocketServer } from 'ws';
 const server = createServer((req, res) => {   // (1)
     return staticHandler(req, res, { public: 'public' })
 });
-const wss = new WebSocketServer({ server }) // (2)
+const wss = new WebSocketServer({ server }) // Actual Server instance shared among all clients
+
+function uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
+
+class CustomClient{
+    constructor(client_ins){
+        this.client_ins = client_ins
+        this.server = wss 
+        this.id = uuidv4()
+    }
+}
+
 wss.on('connection', (client) => {
     console.log('Client connected !')
     client.on('message', (msg) => {    // (3)
@@ -13,6 +28,7 @@ wss.on('connection', (client) => {
         broadcast(msg)
     })
 })
+
 function broadcast(msg) {       // (4)
     for (const client of wss.clients) {
         if (client.readyState === ws.OPEN) {
@@ -20,6 +36,7 @@ function broadcast(msg) {       // (4)
         }
     }
 }
+
 server.listen(process.argv[2] || 8080, () => {
     console.log(`server listening...`);
 })
